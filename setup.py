fbc44571f4913259d348477ee3ab4a4e26d8f1ef
Maxime Madrau <maxime.madrau@icloud.com>
2024-09-19 13:37:37 +0200

    Update package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="PyTouchBar",
-    version="0.2.0",
+    version="0.2.1",
     author="Maxmad68",
     author_email="maxime@madrau.fr",
     description="Use MacBook Pro's TouchBar in Python",
